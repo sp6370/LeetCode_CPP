0e0f40e38dd3ae1d336c51ebf9116433508bc802
Sudhanshu Pandey <sp6370@nyu.edu>
2022-03-02 22:32:31 -0500

    Time: 6 ms (65.96%), Space: 8.3 MB (42.94%) - LeetHub

--- 338-counting-bits/338-counting-bits.cpp
+++ 338-counting-bits/338-counting-bits.cpp
@@ -1,22 +1,28 @@
 class Solution {
 public:
     vector<int> countBits(int n) {
-        
-        vector<int> result(n);
-        int offsetValue = 1;
-        
-        result.push_back(0);
+        vector<int> DP(n);
+        int offset = 1;
+        DP.push_back(0);
+        // 0: 0 0 0
+        // 1: 0 0 1
+        // 2: 0 1 0
+        // 3: 0 1 1
+        // 4: 1 0 0
+        // 5: 1 0 1
+        // 6: 1 1 0
+        // 7: 1 1 1
         
         for(int i=1; i<=n; i++)
         {
-            if(offsetValue * 2 == i)
+            if(offset * 2 == i)
             {
-                offsetValue = i;
+                offset = i;
             }
             
-            result[i] = 1 + result[i - offsetValue];
+            DP[i] = 1 + DP[i - offset];
         }
         
-        return result;
+        return DP;
     }
 };
