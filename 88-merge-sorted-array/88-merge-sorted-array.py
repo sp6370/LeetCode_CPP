85d15923ecab2bd0ead0a5ddb2ef5a6c123b6a5c
Sudhanshu Pandey <sp6370@nyu.edu>
2022-03-25 19:58:58 -0400

    Time: 46 ms (67.72%), Space: 13.8 MB (88.97%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.py
+++ 88-merge-sorted-array/88-merge-sorted-array.py
@@ -11,7 +11,6 @@ class Solution:
         
         """
         Using 3 pointer along with extra space
-        """
         copy_nums1 = copy.deepcopy(nums1[:m])
         
         #read pointers
@@ -28,6 +27,25 @@ class Solution:
             else:
                 nums1[i] = nums2[read_nums2]
                 read_nums2+=1
+        """
+        
+        # Approach 3: Using 3 pointers and no extra space
+        # Let's exploit the space available at the end of the array
+        
+        #read pointers
+        p1 = m-1
+        p2 = n-1
+        
+        #write pointer
+        p3 = m+n-1
+        
+        for p3 in range(m+n-1, -1, -1):
+            if p2<0 or (p1>=0 and nums1[p1]>nums2[p2]):
+                nums1[p3] = nums1[p1]
+                p1-=1
+            else:
+                nums1[p3] = nums2[p2]
+                p2-=1
         
         
         
