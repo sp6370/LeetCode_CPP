140387e58987ad977e5339c079efa518df0e10d0
Sudhanshu Pandey <sp6370@nyu.edu>
2022-03-25 19:47:26 -0400

    Time: 51 ms (56.40%), Space: 13.9 MB (45.04%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.py
+++ 88-merge-sorted-array/88-merge-sorted-array.py
@@ -3,13 +3,29 @@ class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
         """
         Do not return anything, modify nums1 in-place instead.
+        Easy Appraoch: Just put them together and sort nums1
         """
+        # for i in range(n):
+        #     nums1[m+i] = nums2[i]
+        # nums1.sort()
         
         """
-        Easy Appraoch: Just put them together in different list and sort it
+        Using 3 pointer along with extra space
         """
-        for i in range(n):
-            nums1[m+i] = nums2[i]
-        nums1.sort()
+        copy_nums1 = copy.deepcopy(nums1[:m])
+        
+        read_copy = 0
+        read_nums2 = 0
+        write_nums1 = 0
+        
+        for i in range(m+n):
+            if (read_nums2 >=n) or (read_copy < m and copy_nums1[read_copy] < nums2[read_nums2]):
+                nums1[i] = copy_nums1[read_copy]
+                read_copy+=1
+            else:
+                nums1[i] = nums2[read_nums2]
+                read_nums2+=1
+        
+        
         
         
