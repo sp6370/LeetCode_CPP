23c00e6c41f3933cfa2e2b2b2d384f2257b28b61
Sudhanshu Pandey <sp6370@nyu.edu>
2022-03-25 20:44:58 -0400

    Time: 52 ms (29.71%), Space: 13.8 MB (99.25%) - LeetHub

---
+++ 157-read-n-characters-given-read4/157-read-n-characters-given-read4.py
@@ -0,0 +1,44 @@
+"""
+The read4 API is already defined for you.
+
+    @param buf4, a list of characters
+    @return an integer
+    def read4(buf4):
+
+# Below is an example of how the read4 API can be called.
+file = File("abcdefghijk") # File is "abcdefghijk", initially file pointer (fp) points to 'a'
+buf4 = [' '] * 4 # Create buffer with enough space to store characters
+read4(buf4) # read4 returns 4. Now buf = ['a','b','c','d'], fp points to 'e'
+read4(buf4) # read4 returns 4. Now buf = ['e','f','g','h'], fp points to 'i'
+read4(buf4) # read4 returns 3. Now buf = ['i','j','k',...], fp points to end of file
+"""
+
+class Solution:
+    def read(self, buf, n):
+        """
+        :type buf: Destination buffer (List[str])
+        :type n: Number of characters to read (int)
+        :rtype: The number of actual characters read (int)
+        """
+        num_calls = -1
+        if n%4 == 0:
+            num_calls = n//4
+        else:
+            num_calls = n//4 + 1
+        
+        ptr = 0
+        tmpBuf = ['']*4
+        
+        for i in range(num_calls):
+            count = read4(tmpBuf)
+            for j in range(count):
+                if ptr+j == n:
+                    ptr = ptr+j
+                    return ptr
+                    break
+                buf[ptr+j] = tmpBuf[j]
+            ptr+=count
+    
+        return ptr
+            
+            
