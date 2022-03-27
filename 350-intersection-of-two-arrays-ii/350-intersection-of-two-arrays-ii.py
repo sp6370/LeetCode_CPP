a07c17b75bc9f6686c6dc9808037b06118291084
Sudhanshu Pandey <sp6370@nyu.edu>
2022-03-27 12:08:13 -0400

    Time: 91 ms (24.33%), Space: 14.1 MB (21.08%) - LeetHub

--- 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.py
+++ 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.py
@@ -1,6 +1,7 @@
 class Solution:
     def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        
+        """
+        Approach using the hash map extra space
         hash_data_1 = {}
         for i in nums1:
             if i in hash_data_1:
@@ -23,4 +24,33 @@ class Solution:
                 result.extend([i]*count)
         
         return result
+        """
         
+        """
+        Sorting and then doing it using two(three) pointers
+        """
+        # if not len(nums1) or not len(nums2):
+        #     return []
+        
+        nums1.sort()
+        nums2.sort()
+        
+        print(nums1)
+        print(nums2)
+        # read pointer 
+        p1 = 0
+        p2 = 0
+        
+        result = []
+        
+        while p1<len(nums1) and p2<len(nums2):
+            if nums1[p1] == nums2[p2]:
+                result.append(nums1[p1])
+                p1+=1
+                p2+=1
+            elif nums1[p1] < nums2[p2]:
+                p1+=1
+            else:
+                p2+=1
+        
+        return result
