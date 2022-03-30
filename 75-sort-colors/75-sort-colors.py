7256b41d147966f051f3a205a156006e631b3221
Sudhanshu Pandey <sp6370@nyu.edu>
2022-03-30 11:45:06 -0400

    Time: 39 ms (74.06%), Space: 14 MB (18.09%) - LeetHub

--- 75-sort-colors/75-sort-colors.py
+++ 75-sort-colors/75-sort-colors.py
@@ -3,11 +3,31 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
+        """
         # let's implement bubble sort
-        
         for i in range(len(nums)-1, -1, -1):
             for j in range(i):
                 if nums[j] > nums[j+1]:
                     nums[j], nums[j+1] = nums[j+1], nums[j]
+        """
+        # simple approach count number of 0, 1, 2
+        
+        data = {}
+        for i in nums:
+            if i in data:
+                data[i]+=1
+            else:
+                data[i]=1
                 
+        for i in range(len(nums)):
+            if 0 in data and data[0] > 0:
+                nums[i] = 0
+                data[0]-=1
+            elif 1 in data and data[1] > 0:
+                nums[i] = 1
+                data[1] -= 1
+            else:
+                nums[i] = 2
+        
+        
                 
