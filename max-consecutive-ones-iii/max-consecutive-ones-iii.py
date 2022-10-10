6b4b41dfe993df47f2cf5b434be494422fb160bc
Sudhanshu Pandey <sp6370@nyu.edu>
2022-10-10 07:37:41 -0400

    Time: 659 ms, Memory: 14.6 MB - LeetHub

---
+++ max-consecutive-ones-iii/max-consecutive-ones-iii.py
@@ -0,0 +1,27 @@
+from collections import defaultdict
+
+class Solution:
+    def longestOnes(self, nums: List[int], k: int) -> int:
+        left = right = max_win_size = 0
+        window = defaultdict(int)
+        n = len(nums)
+        
+        for right in range(n):
+            
+            val = nums[right]
+            
+            if val == 1:
+                window[val] += 1
+                # assumption windows is valid every time at this point
+            else:
+                #val is 0
+                window[val] += 1
+
+                while left <= right and window[0] > k:           
+                    window[nums[left]] -= 1
+                    left += 1
+
+            max_win_size = max(max_win_size, window[0] + window[1])
+        
+        return max_win_size
+            
